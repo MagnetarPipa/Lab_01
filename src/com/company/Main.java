31fa22d36c715a4c51ccbe390edd73a15ab2b50d
MagnetarPipa <GITHUB you@example.com>
2022-03-21 08:58:30 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,43 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Formula();
+        task43();
+    }
+
+    /**
+     * 43.Вычислить у = sin 1 + sin 1,1 + sin 1,2 + ... + sin 2.
+     */
+    private static void task43()
+    {
+        int y=0;
+
+        do{
+            
+        }while()
+
+    }
+
+    /**
+     * Task Number=43
+     * Task Number=68
+     * Task Number=93
+     * Task Number=118
+     * Task Number=143
+     */
+    private static void Formula()
+    {
+        int N=18,G=1;
+        for(int K=1;K<6;K++)
+        {
+
+           int task_number=(24 + N + (K - 1) *25)+G;
+           System.out.println("Task Number=" + task_number + " K="+K);
+        }
+
+
+    }
+
+}
