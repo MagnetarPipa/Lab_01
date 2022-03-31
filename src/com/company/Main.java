2550fc290d799292324bc8c5469eb09ee4930f5b
MagnetarPipa <GITHUB you@example.com>
2022-03-31 16:50:26 +0300

    Task 118 redone

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,11 +11,11 @@ public class Main {
 
 
     public static void main(String[] args) {
-       // task43();
-       // task68();
-       // task93();
+        task43();
+        task68();
+        task93();
         task118();
-       // task143();
+        task143();
     }
 
     /**
